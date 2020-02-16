c91b65aa7be288cc4a3459c08c7161c683675ebe
Olived <dufour.oli@icloud.com>
2020-02-16 23:54:57 +0100

    add db config

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,8 @@
+import os
 from flask.app import Flask
 from flask.helpers import get_debug_flag
 from flask_cors import CORS
-
+from pymongo import MongoClient
 from server.index import server
 from .config import STATIC_FOLDER, TEMPLATE_FOLDER, BaseConfig, DevConfig, ProdConfig
 
@@ -13,10 +14,11 @@ def create_app():
     in which case it uses :class:`backend.config.DevConfig`. Also configures
     paths for the templates folder and static files.
     """
+    dev = get_debug_flag()
     return _create_app(
-        DevConfig if get_debug_flag() else ProdConfig,
-        "development.cfg" if get_debug_flag() else None,
-        instance_relative_config=True if get_debug_flag() else False,
+        DevConfig if dev else ProdConfig,
+        "development.cfg" if dev else None,
+        instance_relative_config=True if dev else False,
         template_folder=TEMPLATE_FOLDER,
         static_folder=STATIC_FOLDER,
     )
@@ -32,10 +34,17 @@ def _create_app(config_object: BaseConfig, config_filename, **kwargs):
     if config_filename:
         app.config.from_pyfile(config_filename)
     register_blueprints(app)
+
+    mongo_db_uri = os.environ.get("MONGODB_URI") or app.config["MONGODB_URI"]
+    if mongo_db_uri is None:
+        app.config["dbClient"] = MongoClient()
+    else:
+        app.config["dbClient"] = MongoClient(mongo_db_uri, ssl=True)
+    app.config["db"] = app.config["dbClient"][os.environ.get("DB") or app.config["DB"]]
+    print(app.config["dbClient"])
     CORS(app, resources={r"/*": {"origins": app.config["CLIENT_ADDR"]}})
     return app
 
 
 def register_blueprints(app):
-
     app.register_blueprint(server)
