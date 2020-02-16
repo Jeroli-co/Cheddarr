f07a9c1b01bc9fd8f593e99fbeba45589dabfd55
Olived <dufour.oli@icloud.com>
2020-02-16 22:16:12 +0100

    Server changes

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-import os
-from server import create_app
-
-app = create_app()
-
 if __name__ == "__main__":
+    import os
+    from server import create_app
+
     if os.environ.get("ENV") == "dev":
         os.environ["FLASK_DEBUG"] = "true"
+
+    app = create_app()
     app.run(host="127.0.0.1", port=5000, debug=True)
