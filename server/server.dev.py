31e91c8b5306cd7ea16293d7f96feca060955a48
Olived <dufour.oli@icloud.com>
2020-02-16 13:34:50 +0100

    Fix static folder path in back

--- server/server.dev.py
+++ server/server.dev.py
@@ -4,14 +4,14 @@ from flask_cors import CORS
 
 CLIENT_ADDR = ["http://127.0.0.1:4200", "http://localhost:4200"]
 app = Flask(
-    __name__, static_folder="../app/build/static", template_folder="../app/build"
+    __name__, static_folder="../client/build/static", template_folder="../client/build"
 )
 CORS(app, resources={r"/*": {"origins": CLIENT_ADDR}})
 
 
 @app.route("/", methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
-def any_root_path(path):
+def any_root_path(path=None):
     return render_template("index.html")
 
 
