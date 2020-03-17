4a092b57290d02d0aa59fa1282eab45d06a12843
Olived <dufour.oli@icloud.com>
2020-03-17 22:23:04 +0100

    Add user_profile.py + user serializer

--- server/auth/routes/__init__.py
+++ server/auth/routes/__init__.py
@@ -1,1 +1,8 @@
-
+from . import (
+    signup,
+    signin,
+    signout,
+    reset_password,
+    change_password,
+    user_profile,
+)  # noqa
---
+++ server/auth/serializers/user_serializer.py
@@ -0,0 +1,13 @@
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+
+from server.auth import User
+
+
+class UserSerializer(SQLAlchemySchema):
+    class Meta:
+        model = User
+        load_instance = True
+
+    first_name = auto_field()
+    last_name = auto_field()
+    username = auto_field()
