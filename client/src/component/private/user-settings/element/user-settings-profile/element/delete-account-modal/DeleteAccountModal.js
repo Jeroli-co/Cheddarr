1f95c1ea2a8f12f77049b636efcc425a96cc04c8
BogetC <bogetcorentin@gmail.com>
2020-03-19 00:07:51 +0100

    Change password, Change username, Delete account

---
+++ client/src/component/private/user-settings/element/user-settings-profile/element/delete-account-modal/DeleteAccountModal.js
@@ -0,0 +1,68 @@
+import React, {useContext, useState} from 'react';
+import {useForm} from "react-hook-form";
+import {AuthContext} from "../../../../../../../context/AuthContext";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faKey} from "@fortawesome/free-solid-svg-icons";
+
+const DeleteAccountModal = ({ isActive, onClose }) => {
+
+  const { register, handleSubmit, reset } = useForm();
+  const { deleteAccount } = useContext(AuthContext);
+  const [status, setStatus] = useState(null);
+
+  const onSubmit = (data) => {
+    deleteAccount(data).then((code) => {
+      if (code !== 200) {
+        setStatus(code);
+      }
+    });
+  };
+
+  const closeModal = () => {
+    reset();
+    onClose();
+  };
+
+  return (
+    <div className={'DeleteAccountModal modal ' + (isActive ? "is-active" : "")} data-testid="DeleteAccountModal">
+      <div className="modal-background" onClick={closeModal} />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Confirm your password</p>
+          <button className="delete" aria-label="close" type="button" onClick={closeModal}/>
+        </header>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <section className="modal-card-body">
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control has-icons-left">
+                  <input name="password"
+                         className="input"
+                         type="password"
+                         placeholder="Enter your password"
+                         ref={register} />
+                  <span className="icon is-small is-left">
+                    <FontAwesomeIcon icon={faKey} />
+                  </span>
+                </div>
+              </div>
+
+              { status && (
+                  (status === 401 && <p className="help is-danger">Wrong password</p>)
+                )
+              }
+
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-danger">Delete account</button>
+            <button className="button" type="button" onClick={closeModal}>Cancel</button>
+          </footer>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export {
+  DeleteAccountModal
+}
