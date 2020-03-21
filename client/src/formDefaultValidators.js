67352e37a65b0c846659a67b12142de5db4da01f
BogetC <bogetcorentin@gmail.com>
2020-03-21 20:09:10 +0100

    [FRONT] WIP: OauthOnly

---
+++ client/src/formDefaultValidators.js
@@ -0,0 +1,8 @@
+const FORM_DEFAULT_VALIDATOR = {
+  REQUIRED: { value: null, message: "This is required" },
+  MAX_LENGTH: { value: 128, message: "Must contain at least 128 characters" }
+};
+
+export {
+  FORM_DEFAULT_VALIDATOR
+}
