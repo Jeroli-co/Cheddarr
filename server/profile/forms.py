884bf52001db4fb040a151ad511cc21299e4036b
Olived <dufour.oli@icloud.com>
2020-03-24 15:20:43 +0100

    Change username and password form validation

--- server/profile/forms.py
+++ server/profile/forms.py
@@ -1,17 +1,26 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
 from wtforms import PasswordField, StringField, FileField
-from wtforms.validators import Length, InputRequired
+from wtforms.validators import Length, InputRequired, Regexp
 
 
 class ChangePasswordForm(FlaskForm):
-    oldPassword = PasswordField("Old Password")
-    newPassword = PasswordField("New Password", [InputRequired(), Length(min=8, max=128)])
+    oldPassword = PasswordField(
+        "Old Password", [InputRequired(), Length(min=8, max=128)]
+    )
+    newPassword = PasswordField(
+        "New Password", [InputRequired(), Length(min=8, max=128)]
+    )
 
 
 class ChangeUsernameForm(FlaskForm):
-    newUsername = StringField("New Username", [InputRequired(), Length(min=4, max=128)])
+    newUsername = StringField(
+        "New Username",
+        [InputRequired(), Length(min=4, max=128), Regexp(r"^[a-zA-Z0-9]+$")],
+    )
 
 
 class PictureForm(FlaskForm):
-    picture = FileField("Picture", [FileRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
+    picture = FileField(
+        "Picture", [FileRequired(), FileAllowed(["jpg", "jpeg", "png", "gif"])]
+    )
