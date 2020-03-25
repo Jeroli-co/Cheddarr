540b7be40a55b3287b250587276be1cd15c6aacc
BogetC <bogetcorentin@gmail.com>
2020-03-25 17:04:37 +0100

    [FRONT] Add friend via search input

--- client/src/component/private/user-profile/element/user-friends-list/UserFriendsList.js
+++ client/src/component/private/user-profile/element/user-friends-list/UserFriendsList.js
@@ -1,15 +1,18 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {AuthContext} from "../../../../../context/AuthContext";
 import './UserFriendsList.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMinus} from "@fortawesome/free-solid-svg-icons";
+import {faMinus, faPlus, faSearch} from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-router-dom";
 import {routes} from "../../../../../routes";
 
 const UserFriendsList = () => {
 
-  const { getFriends, deleteFriend } = useContext(AuthContext);
+  const { getFriends, addFriend, deleteFriend } = useContext(AuthContext);
   const [friends, setFriends] = useState([]);
+
+  const searchFriendsInput = useRef();
+  const [addFriendsHttpResponse, setAddFriendsHttpResponse] = useState(null);
 
   useEffect(() => {
     getFriends().then(res => {
@@ -34,6 +37,28 @@ const UserFriendsList = () => {
           return;
       }
     })
+  };
+
+  const _onAddFriend = () => {
+    const inputValue = searchFriendsInput.current.value;
+    if (inputValue.replace(/\s/g,'').length > 0) {
+      addFriend(inputValue).then(res => {
+        switch (res.status) {
+          case 200:
+            setAddFriendsHttpResponse(res);
+            console.log(res);
+            return;
+          case 409:
+            setAddFriendsHttpResponse(res);
+            return;
+          case 404:
+            setAddFriendsHttpResponse(res);
+            return;
+          default:
+            return;
+        }
+      });
+    }
   };
 
   const FriendsList = ({ friends }) => {
@@ -74,7 +99,37 @@ const UserFriendsList = () => {
 
   return (
     <div className="UserFriendsList" data-testid="UserFriendsList">
-      <FriendsList friends={friends}/>
+      <div className="container">
+        <div className="level">
+          <div className="level-left">
+            <div className="level-item">
+              <div className="field has-addons">
+                <div className="control has-icons-left">
+                  <input className="input" type="search" ref={searchFriendsInput} placeholder="Add friends" autoComplete="off" />
+                  <span className="icon is-small is-left"><FontAwesomeIcon icon={faSearch} /></span>
+                </div>
+                <div className="control">
+                  <button className="button is-success" onClick={_onAddFriend}>
+                    <span className="icon is-small"><FontAwesomeIcon icon={faPlus} /></span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            { addFriendsHttpResponse &&
+              <div className="level-item">
+                {
+                  (addFriendsHttpResponse.status === 200 && <p className="help is-success">{addFriendsHttpResponse.message}</p>) ||
+                  (addFriendsHttpResponse.status === 409 && <p className="help is-danger">{addFriendsHttpResponse.message}</p>) ||
+                  (addFriendsHttpResponse.status === 404 && <p className="help is-danger">{addFriendsHttpResponse.message}</p>)
+                }
+              </div>
+            }
+          </div>
+        </div>
+        <hr/>
+        <FriendsList friends={friends}/>
+
+      </div>
     </div>
   )
 };
--- client/src/component/private/user-public-profile/UserPublicProfile.js
+++ client/src/component/private/user-public-profile/UserPublicProfile.js
@@ -1,13 +1,11 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router";
 import {AuthContext} from "../../../context/AuthContext";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlus} from "@fortawesome/free-solid-svg-icons";
 
 const UserPublicProfile = () => {
 
   const { id } = useParams();
-  const { isAuthenticated, username, getUserPublic, addFriend } = useContext(AuthContext);
+  const { getUserPublic } = useContext(AuthContext);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -23,17 +21,6 @@ const UserPublicProfile = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const _onAddUser = () => {
-    addFriend(user.username).then(res => {
-      switch (res.status) {
-        case 200:
-          return;
-        default:
-          return;
-      }
-    });
-  };
-
   return (
     <div className="UserPublicProfile container has-text-centered" data-testid="UserPublicProfile">
 
@@ -44,17 +31,6 @@ const UserPublicProfile = () => {
           <img src={user["user_picture"]} alt="User" width={260} height={260} data-testid="UserPublicProfileImage" />
           <p className="is-size-5" data-testid="UserPublicProfileUsername"><i>{'@' + user.username}</i></p>
         </div>
-      }
-
-      <br/>
-
-      { isAuthenticated && id !== username &&
-        <button className="button is-primary" type="button" onClick={_onAddUser}>
-          <span className="icon">
-            <FontAwesomeIcon icon={faPlus}/>
-          </span>
-          <span>Add friend</span>
-        </button>
       }
 
     </div>
