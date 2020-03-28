49f53027a1a863944bd64496ab7c55d5f80abfc8
BogetC <bogetcorentin@gmail.com>
2020-03-29 00:36:48 +0100

    [FRONT] Friends style for large screen

--- client/src/modules/profile/friends/Friends.js
+++ client/src/modules/profile/friends/Friends.js
@@ -12,28 +12,26 @@ const Friends = () => {
 
   return (
     <div className="Friends" data-testid="Friends">
-      <div className="container">
-        <AddFriendsInput/>
-        <hr/>
-        <div className="tile is-ancestor">
-          <div className="tile is-6 is-parent">
-            <div className="tile is-child">
-              <h5 className="subtitle is-5">Friends ({getFriendsLength()})</h5>
-              <hr/>
-              <FriendsList/>
-            </div>
+      <AddFriendsInput/>
+      <hr/>
+      <div className="tile is-ancestor">
+        <div className="tile is-6 is-parent">
+          <div className="tile is-child">
+            <h5 className="subtitle is-5">Friends ({getFriendsLength()})</h5>
+            <hr/>
+            <FriendsList/>
           </div>
-          <div className="tile is-vertical is-parent">
-            <div className="tile is-child">
-              <h5 className="subtitle is-5">Requested ({getRequestedLength()})</h5>
-              <hr/>
-              <RequestedList/>
-            </div>
-            <div className="tile is-child">
-              <h5 className="subtitle is-5">Received ({getReceivedLength()})</h5>
-              <hr/>
-              <ReceivedList/>
-            </div>
+        </div>
+        <div className="tile is-vertical is-parent">
+          <div className="tile is-child">
+            <h5 className="subtitle is-5">Requested ({getRequestedLength()})</h5>
+            <hr/>
+            <RequestedList/>
+          </div>
+          <div className="tile is-child">
+            <h5 className="subtitle is-5">Received ({getReceivedLength()})</h5>
+            <hr/>
+            <ReceivedList/>
           </div>
         </div>
       </div>
