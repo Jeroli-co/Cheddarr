315a82742daba9d02859ddc5666aa9b86fd489d1
BogetC <bogetcorentin@gmail.com>
2020-04-07 17:51:55 +0200

    Redirect after sign in

--- client/src/modules/auth/authorize/AuthorizePlex.js
+++ client/src/modules/auth/authorize/AuthorizePlex.js
@@ -7,19 +7,8 @@ const AuthorizePlex = (props) => {
 	const { authorizePlex } = useContext(AuthContext);
 
 	useEffect(() => {
-		authorizePlex(props.location.search).then((res) => {
-			if (res) {
- 				switch (res.status) {
- 					case 200:
- 						let redirectURI = res.headers["redirect-uri"];
- 						redirectURI = redirectURI.length > 0 ? redirectURI : routes.HOME.url;
- 						props.history.push(redirectURI);
- 						return;
- 					default:
- 						return null;
- 				}
- 			}
-		});
+		console.log("Authorizing plex");
+		authorizePlex(props.location.search);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
