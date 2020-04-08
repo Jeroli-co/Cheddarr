9e2d3f2834611a367ee8930617906a6a08367994
Olived <dufour.oli@icloud.com>
2020-04-08 22:26:48 +0200

    Update app.json for Heroku + update FLASK_DOMAIN

--- server/config.py
+++ server/config.py
@@ -71,7 +71,9 @@ class ProdConfig(Config):
     ##########################################################################
     # flask                                                                  #
     ##########################################################################
-    FLASK_DOMAIN = os.environ.get("FLASK_DOMAIN")
+    FLASK_DOMAIN = os.environ.get(
+        "FLASK_DOMAIN", os.environ.get("HEROKU_APP_NAME") + ".herokuapp.com"
+    )
 
     ##########################################################################
     # session/cookies                                                        #
