6a4f9eade5619f3d9c5b32b329e5489d6b2cacfd
Olived <dufour.oli@icloud.com>
2020-04-13 00:21:43 +0200

    WIP Recent Plex movies

--- server/providers/serializers/media_serializer.py
+++ server/providers/serializers/media_serializer.py
@@ -1,16 +1,41 @@
-from marshmallow import fields, post_load
+from marshmallow import fields, post_dump, pre_dump
 
 from server.extensions import ma
-from server.providers.models.media import Movie
 
 
-class PlexMovieSerializer(ma.Schema):
-    title = fields.Str(data_key="title")
+class PlexMediaTag(ma.Schema):
+    tag = fields.String()
+
+
+tag_field = fields.List(fields.Pluck(PlexMediaTag, "tag"))
+
+
+class PlexVideoSerializer(ma.Schema):
+    title = fields.String()
     thumbUrl = fields.URL(data_key="poster")
+    summary = fields.String()
+    duration = fields.String()
+    actors = tag_field
+    rating = fields.String()
+    webUrl = fields.String()
 
-    @post_load
-    def make_movie(self, data, **kwargs):
-        return Movie(**data)
+    @pre_dump
+    def plex_media_web_url(self, media, **kwargs):
+        media.webUrl = "%s/web/index.html#!/server/%s/details?key=%s" % (
+            media._server._baseurl,
+            media._server.machineIdentifier,
+            media.key,
+        )
+        return media
+
+
+class PlexMovieSerializer(PlexVideoSerializer):
+    directors = tag_field
+
+
+class PlexSeriesSerializer(PlexVideoSerializer):
+    seasonNumber = fields.Function(lambda ep: ep.seasonNumber)
 
 
 plex_movie_serializer = PlexMovieSerializer()
+plex_series_serializer = PlexSeriesSerializer()
