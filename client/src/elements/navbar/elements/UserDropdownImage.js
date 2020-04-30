29aef0b87db837b3be85fb0dc6cca1cff609ec49
Boget Corentin <bogetcorentin@gmail.com>
2020-04-30 21:27:16 +0200

    [FRONT] WIP: search bar

--- client/src/elements/navbar/elements/UserDropdownImage.js
+++ client/src/elements/navbar/elements/UserDropdownImage.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 const UserDropdownImage = styled.div`
-  width: 60px;
-  height: 60px;
   img {
     border-radius: 50%;
+    min-height: 60px;
+    max-height: 60px;
+    min-width: 60px;
+    max-width: 60px;
   }
 `;
 
