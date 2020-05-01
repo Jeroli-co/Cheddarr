99b2d8e51d3da54f17a61f0bd1de12d7ce4ecbf3
Olived <dufour.oli@icloud.com>
2020-05-01 18:16:22 +0200

    Search on all friends, movies and series

--- server/search/views.py
+++ server/search/views.py
@@ -1,63 +1,61 @@
 from http import HTTPStatus
 
-from flask import request
+from flask import jsonify, request
 from flask_login import current_user, login_required
 from sqlalchemy import or_
 
 from server.auth.models import User
 from server.exceptions import HTTPError
-from server.profile.forms import UsernameOrEmailForm
-from server.profile.serializers import profiles_serializer
 from server.providers.utils import plex
 from server.search import search
-from server.search.forms import MediaSearchForm, UserSearchForm
-from server.search.serializers import media_search_serializer
+from server.search.forms import SearchForm
+from server.search.serializers import media_search_serializer, friends_search_serializer
 
 
-@search.route("/friends/")
+@search.route("/")
 @login_required
-def search_friends():
-    username_or_email = UserSearchForm(request.args)
-    if not username_or_email.validate():
+def search():
+    search_form = SearchForm(request.args)
+    if not search_form.validate():
         raise HTTPError(
-            "Error while searching user.",
+            "Error while searching.",
             status_code=HTTPStatus.BAD_REQUEST,
-            payload=username_or_email.errors,
+            payload=search_form.errors,
         )
+    search_type = search_form.type.data
+    if search_type == "friends":
+        return jsonify(search_friends(search_form))
+    if search_type == "movies" or search_type == "series":
+        return jsonify(search_media(search_form))
+    return jsonify(search_friends(search_form) + search_media(search_form))
+
+
+def search_friends(search_form: SearchForm):
+    value = search_form.value.data
     result = []
     for user in (
         User.query.filter(
-            or_(
-                User.username.contains(username_or_email.usernameOrEmail.data),
-                User.email.contains(username_or_email.usernameOrEmail.data),
-            )
+            or_(User.username.contains(value), User.email.contains(value),)
         )
         .limit(5)
         .all()
     ):
         if user in current_user.friends_received or user in current_user.friends_sent:
             result.append(user)
-    return profiles_serializer.jsonify(result, many=True)
+    return friends_search_serializer.dump(result)
 
 
-@search.route("/media/")
-@login_required
-def search_movies():
-    media_search = MediaSearchForm(request.args)
-    if not media_search.validate():
-        raise HTTPError(
-            "Error while searching movies.",
-            status_code=HTTPStatus.BAD_REQUEST,
-            payload=media_search.errors,
-        )
+def search_media(search_form: SearchForm):
     plex_server = plex.user_server(current_user)
+    value = search_form.value.data
+    type = search_form.type.data
     filters = {
-        field: value for field, value in media_search.data.items() if field != "type"
+        field: value
+        for field, value in search_form.data.items()
+        if field != "type" and field != "value"
     }
 
     if plex_server is None:
         raise HTTPError("No Plex server linked.", status_code=HTTPStatus.BAD_REQUEST)
-    result = plex.search(
-        plex_server, section_type=media_search.type.data, filters=filters
-    )
-    return media_search_serializer.jsonify(result), HTTPStatus.OK
+    result = plex.search(plex_server, section_type=type, title=value, filters=filters)
+    return media_search_serializer.dump(result)
