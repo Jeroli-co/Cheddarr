097a2d338fc08ea49ddfde4f61785d892fb3f1fb
Olived <dufour.oli@icloud.com>
2020-05-29 17:38:21 +0200

    Remove ResetPassword.test.js and fix imports

--- client/src/modules/auth/reset-password/reset-password-form/ResetPasswordForm.js
+++ client/src/modules/auth/reset-password/reset-password-form/ResetPasswordForm.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
 import { useForm } from "react-hook-form";
