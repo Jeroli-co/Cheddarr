460f245d8fe6d71d4faa5a96bd304e39b084c87b
Olived <dufour.oli@icloud.com>
2020-05-29 17:38:21 +0200

    Remove ResetPassword.test.js and fix imports

--- client/src/modules/auth/reset-password/ResetPassword.js
+++ client/src/modules/auth/reset-password/ResetPassword.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { TokenExpired } from "../elements/TokenExpired";
 import { ResetPasswordForm } from "./reset-password-form/ResetPasswordForm";
