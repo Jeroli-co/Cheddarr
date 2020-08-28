adbde1b3158e156bc2d440c82ce888219ab0b422
Boget Corentin <bogetcorentin@gmail.com>
2020-08-28 16:32:55 +0200

    WIP: rework sign up

--- client/src/modules/auth/sign-up-form/AvatarInput.js
+++ client/src/modules/auth/sign-up-form/AvatarInput.js
@@ -12,56 +12,56 @@ import cheeseRed from "../../../assets/profile/cheese-red.png";
 import { Image } from "../../../elements/Image";
 import { RowLayout } from "../../../elements/layouts";
 
+const avatars = [
+  { title: "cheese-cyan.png", src: cheeseCyan, description: "Cyan cheese" },
+  {
+    title: "cheese-ocean.png",
+    src: cheeseOcean,
+    description: "Ocean cheese",
+  },
+  { title: "cheese-blue.png", src: cheeseBlue, description: "Blue cheese" },
+  {
+    title: "cheese-green.png",
+    src: cheeseGreen,
+    description: "Green cheese",
+  },
+  {
+    title: "cheese-orange.png",
+    src: cheeseOrange,
+    description: "Orange cheese",
+  },
+  { title: "cheese-red.png", src: cheeseRed, description: "Red cheese" },
+  {
+    title: "cheese-purple.png",
+    src: cheesePurple,
+    description: "Purple cheese",
+  },
+  { title: "cheese-pink.png", src: cheesePink, description: "Pink cheese" },
+];
+
 const AvatarInput = ({ onPrevious, onValidInput }) => {
-  const avatars = [
-    { title: "cheese-cyan.png", src: cheeseCyan, description: "Cyan cheese" },
-    {
-      title: "cheese-ocean.png",
-      src: cheeseOcean,
-      description: "Ocean cheese",
-    },
-    { title: "cheese-blue.png", src: cheeseBlue, description: "Blue cheese" },
-    {
-      title: "cheese-green.png",
-      src: cheeseGreen,
-      description: "Green cheese",
-    },
-    {
-      title: "cheese-orange.png",
-      src: cheeseOrange,
-      description: "Orange cheese",
-    },
-    { title: "cheese-red.png", src: cheeseRed, description: "Red cheese" },
-    {
-      title: "cheese-purple.png",
-      src: cheesePurple,
-      description: "Purple cheese",
-    },
-    { title: "cheese-pink.png", src: cheesePink, description: "Pink cheese" },
-  ];
+  const [selectedImage, setSelectedImage] = useState(avatars[0].title);
 
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
-
-  const isSelected = (index) => {
-    return index === selectedImageIndex;
+  const isSelected = (title) => {
+    return title === selectedImage;
   };
 
   return (
-    <form onSubmit={() => onValidInput(avatars[selectedImageIndex].title)}>
+    <form onSubmit={() => onValidInput(selectedImage)}>
       <RowLayout childMargin="10px" wrap="wrap">
-        {avatars.map((avatar, index) => (
+        {avatars.map((avatar) => (
           <Image
-            key={index}
+            key={avatar.title}
             src={avatar.src}
             alt={avatar.description}
             width="100px"
             height="100px"
-            opacity={isSelected(index) ? "1" : "0.5"}
+            opacity={isSelected(avatar.title) ? "1" : "0.5"}
             hoverOpacity="1"
             cursor="pointer"
-            border={isSelected(index) ? "5px solid #00cc00 " : "none"}
-            borderRadius={isSelected(index) ? "100px " : "0"}
-            onClick={() => setSelectedImageIndex(index)}
+            border={isSelected(avatar.title) ? "5px solid #00cc00 " : "none"}
+            borderRadius={isSelected(avatar.title) ? "100px " : "0"}
+            onClick={() => setSelectedImage(avatar.title)}
           />
         ))}
       </RowLayout>
