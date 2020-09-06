102e818ebd35c9fe6bd2797573e658e2e9cabd97
Olived <oli.dufour@outlook.com>
2020-09-07 01:19:57 +0200

    WIP online media search

--- server/api/search/tmdb/__init__.py
+++ server/api/search/tmdb/__init__.py
@@ -1,4 +1,8 @@
 import tmdbsimple as tmdb
 
+tmdb_config = tmdb.Configuration().info()
 tmdb_movies = tmdb.Movies()
 tmdb_search = tmdb.Search()
+
+tmdb_images_url = tmdb_config.get("images").get("secure_base_url")
+tmdb_poster_sizes = tmdb_config.get("images").get("poster_sizes")
