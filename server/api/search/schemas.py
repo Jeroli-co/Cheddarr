33302cf80e183a3cca08a8069e005f0183f50541
Olived <oli.dufour@outlook.com>
2020-09-07 15:49:40 +0200

    Done basic online media search

--- server/api/search/schemas.py
+++ server/api/search/schemas.py
@@ -1,4 +1,4 @@
-from marshmallow import pre_dump
+from marshmallow import post_dump
 from marshmallow.validate import OneOf
 from server.api.auth.models import User
 from server.extensions import ma
@@ -17,9 +17,9 @@ class MediaSearchResultSchema(ma.Schema):
     thumbUrl = ma.String()
     type = ma.String()
 
-    @pre_dump
+    @post_dump
     def media_type(self, media, **kwargs):
-        media.type = media.type.replace("show", "series")
+        media["type"] = media.get("type").replace("show", "series")
         return media
 
 
