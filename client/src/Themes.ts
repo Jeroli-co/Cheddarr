d156179d71f3232de8a53bf5b0158520227183d9
olived <oli.dufour@outlook.com>
2021-03-14 19:46:49 +0100

    [front] Change some colors

--- client/src/Themes.ts
+++ client/src/Themes.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "styled-components";
 
 export const THEMES: DefaultTheme = {
   bgColor: "#0D1117",
-  color: "#fffff7",
+  color: "#fdfdf7",
   black: "hsl(214, 24%, 0%)",
   primary: "hsl(214, 24%, 10%)",
   primaryLight: "hsl(214, 24%, 20%)",
@@ -11,11 +11,11 @@ export const THEMES: DefaultTheme = {
   secondary: "hsl(23, 99%, 48%)",
   success: "hsl(129, 30%, 52%)",
   danger: "hsl(0, 91%, 52%)",
-  warning: "hsl(38, 91%, 49%)",
+  warning: "hsl(38,91%,49%)",
   grey: "#a1978a",
   plex: "#282a2d",
-  movie: "hsl(185, 100%, 35%)",
+  movie: "hsl(223,81%,62%)",
   series: "hsl(259, 50%, 50%)",
-  season: "hsl(0,69%,48%)",
-  episode: "hsl(336, 69%, 48%)",
+  season: "hsl(17,71%,51%)",
+  episode: "hsl(174,78%,32%)",
 };
