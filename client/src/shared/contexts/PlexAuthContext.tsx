aa5ca7f108a479c912df238c564d3e1e30b094e1
BogetC <bogetcorentin@gmail.com>
2021-06-19 00:52:10 +0200

    WIP: fix plex 404

--- client/src/shared/contexts/PlexAuthContext.tsx
+++ client/src/shared/contexts/PlexAuthContext.tsx
@@ -1,10 +1,10 @@
-import React, {createContext, useContext} from "react";
-import {APIRoutes} from "../enums/APIRoutes";
-import {instance} from "../../axiosInstance";
-import {ERRORS_MESSAGE} from "../enums/ErrorsMessage";
-import {useAPI} from "../hooks/useAPI";
-import {useAlert} from "./AlertContext";
-import {useSession} from "./SessionContext";
+import React, { createContext, useContext } from "react";
+import { APIRoutes } from "../enums/APIRoutes";
+import { instance } from "../../axiosInstance";
+import { ERRORS_MESSAGE } from "../enums/ErrorsMessage";
+import { useAPI } from "../hooks/useAPI";
+import { useAlert } from "./AlertContext";
+import { useSession } from "./SessionContext";
 
 interface IPlexAuthContextInterface {
   readonly signInWithPlex: (redirectURI?: string) => void;
@@ -32,17 +32,32 @@ export default function PlexAuthContextProvider(props: any) {
       if (res1.data) {
         instance
           .post<{ id: string; code: string }>(res1.data, {
-            headers: { Accept: "application/json" },
+            headers: {
+              Accept: "application/json",
+              "Cache-Control": "no-cache",
+              Pragma: "no-cache",
+              Expires: "0",
+            },
           })
           .then(
             (res2) => {
               instance
-                .post(APIRoutes.AUTHORIZE_PLEX_SIGN_IN, {
-                  key: res2.data.id,
-                  code: res2.data.code,
-                  redirect_uri: redirectURI ? redirectURI : "",
-                  user_id: user?.id,
-                })
+                .post(
+                  APIRoutes.AUTHORIZE_PLEX_SIGN_IN,
+                  {
+                    key: res2.data.id,
+                    code: res2.data.code,
+                    redirect_uri: redirectURI ? redirectURI : "",
+                    user_id: user?.id,
+                  },
+                  {
+                    headers: {
+                      "Cache-Control": "no-cache",
+                      Pragma: "no-cache",
+                      Expires: "0",
+                    },
+                  }
+                )
                 .then(
                   (res3) => {
                     window.location.href = res3.headers.location;
